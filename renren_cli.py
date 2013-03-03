acde7784bfba2e1617367cb1b13a95e933ca90c5
Timothy Qiu <timothyqiu32@gmail.com>
2013-03-03 16:34:50 +0800

    [Ref]: Encryptor moved to seperate file. [Ref]: Naming convention.

--- renren_cli.py
+++ renren_cli.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-#
-# Reference: http://s.xnimg.cn/a52197/n/apps/login/login-v6.js
+# vim:fileencoding=utf-8
 
 from datetime import datetime
 import cookielib
@@ -12,73 +10,15 @@ import json
 import urllib
 import urllib2
 
-
-class LoginEncryptor:
-
-    URL_GET_ENCRYPT_KEY = 'http://login.renren.com/ajax/getEncryptKey'
-
-    CACHE_FILENAME = '.renren.login.encryptor.cache'
-
-    def __init__(self):
-        if os.path.exists(self.CACHE_FILENAME):
-            self.loadKeys()
-        else:
-            self.retrieveKeys()
-
-    def saveKeys(self, keys):
-        with open(self.CACHE_FILENAME, 'wb') as f:
-            json.dump(keys, f)
-
-    def loadKeys(self):
-        if os.path.exists(self.CACHE_FILENAME):
-            with open(self.CACHE_FILENAME, 'rb') as f:
-                keys = json.load(f)
-            self.update(keys)
-        else:
-            logging.warn('%s does not exist.' % self.CACHE_FILENAME)
-
-    def retrieveKeys(self):
-        response = urllib2.urlopen(self.URL_GET_ENCRYPT_KEY)
-        res = json.loads(response.read())
-        self.update(res)
-        self.saveKeys(res)
-
-    def update(self, res):
-        self.rkey = res['rkey']
-        self.e = int(res['e'], 16)
-        self.n = int(res['n'], 16)
-        self.chunkSize = self.getChunkSize(res['n'])
-
-    def getChunkSize(self, n):
-        for s in range(len(n) - 4, 0, -4):
-            if int(n[s:s+4], 16) != 0:
-                return s / 2
-        return 0
-
-    def encryptedString(self, value):
-        # value to binary array and pad zero according to chunk size
-        a = map(ord, value)
-        a.extend([0] * (self.chunkSize - (len(a) % self.chunkSize)))
-
-        # divide to fixed size chunks
-        chunks = zip(* [iter(a)] * self.chunkSize)
-
-        # hash chunks
-        h = []
-        for chunk in chunks:
-            # make c integer form of a
-            c = sum(chunk[i] << (i*8) for i in range(self.chunkSize))
-
-            h.append('%x' % pow(c, self.e, self.n))
-        return ' '.join(h)
+from renren_encryptor import LoginEncryptor
 
 
 class RenrenNotification:
 
     def __init__(self, data):
-        self.parseJSON(data)
+        self.parse_json(data)
 
-    def parseJSON(self, ntf):
+    def parse_json(self, ntf):
         timestamp = float(ntf['time'])
         content = ntf['content']
 
@@ -186,8 +126,8 @@ class RenrenClient:
     def login(self):
         data = {
             'email': self.email,
-            'password': self.encryptor.encryptedString(self.password),
-            'rkey': self.encryptor.rkey,
+            'password': self.encryptor.encrypt(self.password),
+            'rkey': self.encryptor.key['rkey'],
             'icode': self.getICode(),
             'origURL': 'http://www.renren.com/home',
             'key_id': '1',
---
+++ renren_encryptor.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python2
+# vim:fileencoding=utf-8
+
+"""Encryption Module for RenrenCLI
+"""
+
+__all__ = ['LoginEncryptor']
+
+
+import json
+import logging
+import os
+import urllib2
+
+
+class LoginEncryptor:
+    """This is an encryptor which is used to encrypt password when login. It
+    seems to be an RSA Algorithm.
+
+    Reference: http://s.xnimg.cn/a52197/n/apps/login/login-v6.js
+    """
+
+    DEFAULT_KEY_URL = 'http://login.renren.com/ajax/getEncryptKey'
+    DEFAULT_KEY_FILENAME = '.renren.encryptor.login.key'
+
+    def __init__(self):
+        """Initialize the encryptor by trying to get key by the following
+        order:
+
+        1. Local file.
+        2. Retrieve the key from web.
+
+        If got from web, the key will be automatic saved to file."""
+        if os.path.exists(self.DEFAULT_KEY_FILENAME):
+            self.load_key()
+        else:
+            self.retrieve_key()
+            self.save_key()
+
+    def save_key(self, filename=None):
+        if not filename:
+            filename = self.DEFAULT_KEY_FILENAME
+        with open(filename, 'wb') as f:
+            json.dump(self.key, f)
+        logging.info('Encrypt key saved to %s' % filename)
+
+    def load_key(self, filename=None):
+        if not filename:
+            filename = self.DEFAULT_KEY_FILENAME
+        with open(filename, 'rb') as f:
+            self.key = json.load(f)
+        logging.info('Encrypt key loaded from %s' % filename)
+
+    def retrieve_key(self, url=None):
+        if not url:
+            url = self.DEFAULT_KEY_URL
+        response = urllib2.urlopen(url)
+        self.key = json.loads(response.read())
+        logging.info('Encrypt key retrieved from %s' % url)
+
+    def get_chunk_size(self):
+        n = self.key['n']
+        for s in range(len(n) - 4, 0, -4):
+            if int(n[s:s+4], 16) != 0:
+                return s / 2
+        logging.error('Encryptor chunk size == 0')
+
+    def encrypt(self, text):
+        n = int(self.key['n'], 16)
+        e = int(self.key['e'], 16)
+        rkey = self.key['rkey']
+        chunk_size = self.get_chunk_size()
+
+        # value to binary array and pad zero according to chunk size
+        a = map(ord, text)
+        a.extend([0] * (chunk_size - (len(a) % chunk_size)))
+
+        # divide to fixed size chunks
+        chunks = zip(* [iter(a)] * chunk_size)
+
+        # hash chunks
+        cipher = []
+        for chunk in chunks:
+            # make c integer form of a
+            msg = sum(chunk[i] << (i*8) for i in range(chunk_size))
+
+            cipher.append('%x' % pow(msg, e, n))
+        return ' '.join(cipher)
+
+
+if __name__ == '__main__':
+    pair = (
+        'example.string',
+        '2db4efad86a2db989c8b41adef828afb107e4e9866f23d461d1970e08269c669'
+    )
+    encryptor = LoginEncryptor()
+    assert encryptor.encrypt(pair[0]) == pair[1], 'Wrong algorithm'
+
+    print "Don't Panic! Everything is OK."
