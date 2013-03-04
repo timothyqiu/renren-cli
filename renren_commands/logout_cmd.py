205d1b0b673c6f63e7d73cf9d23c8bd091046e8d
Timothy <timothyqiu32@gmail.com>
2013-03-05 03:28:09 +0800

    [Add]: Logout command.

---
+++ renren_commands/logout_cmd.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+# vim:fileencoding=utf-8
+
+"""Command Implementation for `logout`
+"""
+
+__all__ = ['make_subparser']
+
+
+import argparse
+import os
+import sys
+
+from renren_client import Client
+
+
+def logout(args):
+    root = os.path.abspath(os.path.dirname(sys.argv[0]))
+    path = os.path.join(root, Client.DEFAULT_COOKIES_FILENAME)
+
+    if os.path.exists(path):
+        os.remove(path)
+        print 'Logged out.'
+    else:
+        print 'Not logged in. Nothing to do.'
+
+
+def make_subparser(subparsers):
+    parser = subparsers.add_parser('logout')
+    parser.set_defaults(func=logout)
+
+
+if __name__ == '__main__':
+    pass
